35f789a073fe1079b0afaff8bf77af0afdd6f081
stef181203 <petrovskistefan90@gmail.com>
2025-06-09 18:48:07 +0200

    Improving the UI look

--- frontend/nutrition-ui/src/App.jsx
+++ frontend/nutrition-ui/src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
 import Layout from "./ui/components/layout/Layout.jsx";
-import Header from "./ui/components/header/Header.jsx";
+import {Box, createTheme, ThemeProvider} from "@mui/material";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: `'Montserrat', sans-serif`,
+    },
+});
 
 const App = () => {
     return (
-        <>
-            <Header></Header>
-            <Layout></Layout>
-        </>
+        <ThemeProvider theme={theme}>
+            <Box sx={{
+                minWidth: '100%',
+                minHeight: '100vh',
+                background: 'linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(0, 212, 255, 1) 100%)'
+            }}>
+                <Layout></Layout>
+            </Box>
+        </ThemeProvider>
     );
 }
 
--- frontend/nutrition-ui/src/ui/pages/HomePage.jsx
+++ frontend/nutrition-ui/src/ui/pages/HomePage.jsx
@@ -1,13 +1,46 @@
-import {Container, Typography} from "@mui/material";
+import {Box, Button, Container, TextField, Typography} from "@mui/material";
 
 
 const HomePage = () => {
     return (
       <>
-          <Container sx={{border: '0.15em solid gray', borderRadius: '10px'}} maxWidth={'lg'}>
-              <Typography>
-                  Trying everything out...
-              </Typography>
+          <Typography variant={'h3'} textAlign={'center'}
+                      sx={{
+                          pt: '0.7em',
+                          color: 'white',
+                          fontWeight: 'bold',
+                          textShadow: '3px 3px 15px rgba(0,0,0,0.5)'
+                      }}
+          >
+              Please provide a meal description
+          </Typography>
+          <Container sx={{
+              backgroundColor: 'white',
+              borderRadius: '5px',
+              minHeight: '70vh',
+              position: 'relative',
+              top: '7%',
+              boxShadow: '3px 3px 15px rgba(0,0,0,0.5)',
+          }}
+          maxWidth={'sm'}>
+              <Box minHeight={'50%'} minWidth={'100%'} sx={{
+                  justifyContent: 'start',
+                  alignItems: 'center',
+                  display: 'flex',
+                  flexDirection: 'column'}}
+              >
+                  <TextField
+                      multiline
+                      minRows={5}
+                      placeholder="Description"
+                      sx={{ minWidth: '90%', marginTop: '3em' }}
+                  />
+                  <Button variant={'contained'} sx={{marginTop: '2em', backgroundColor: '#0000A3'}}>Submit</Button>
+              </Box>
+              <Box minHeight={'50%'}>
+
+              </Box>
+
           </Container>
       </>
     );
